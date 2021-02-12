bffc969780948fc68671bfc605c9f9e07fd8d4da
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2021-02-12 17:29:02 +0000

    Project page created. Data method set with project data object, head method set for project page title, html template created

---
+++ pages/projects.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h1>Projects</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Projects',
+      projects: [
+        {
+          id: 1,
+          title: 'UniTap',
+          detail: 'A water refill point locator for University campus in a affect to reduce single use plastics',
+          link: 'https://github.com/clarkenewsh/Uni-Tap',
+          thumbnail: ''
+        }
+
+      ]
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  },
+  methods: {
+    function () {
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  grid-column: 2;
+}
+
+</style>
