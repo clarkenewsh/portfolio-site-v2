ab2fa459019fff740e0b3ef55f10583553a23f4b
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-04-26 16:33:49 +0100

    nuxt generate build command

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,6 @@
     <h1>A little about me</h1>
     <p>Hey, I'm Clarke Newsham, aspiring Web Developer and Software Tester from the North West, UK. I'm super passionate about tech and how we can embrace it to develop great things for humanity.</p>
     <p>Curiosity and futuristic thinking are what keeps me ambitious to design and develop web apps accessible to all, using the skills and techniques I have learned over the last 4 years. I love to stay creative and up to date with emerging tech and development techniques by getting hands dirty building side projects and University studies. Please view my resume and check out my projects below! </p>
-    <img src="~/assets/about-feature-img.jpg">
   </section>
 </template>
 
