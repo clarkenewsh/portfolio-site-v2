03fff6d31e3f981043db7bf10eff852507a3cef8
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-05-05 22:08:47 +0100

    case study content added

--- pages/projects/casestudy1.vue
+++ pages/projects/casestudy1.vue
@@ -5,9 +5,37 @@
       <p>A live client brief designed and devloped for a local Dog walking company</p>
     </div>
     <div>
-      <h2>The process and my workflow</h2>
-      <p>timeline of workflow here seperating my dev workflow and my software tester workflow</p>
-      <h2>The solution</h2>
+      <div id="workflow">
+        <div>
+          <h2>The process and my workflow</h2>
+          <p>My workflow is much more than just the development. Identifying your goals, understanding your user base to create the best product to solve their goals is how I like to do things.</p>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h3>Reserach &#x26; Dicovery</h3>
+              <p>Before I start a project I like to find out about your user base a conduct user disocovery to understand them</p>
+            </li>
+            <li>
+              <h3>Design</h3>
+              <p>Before I start a project I like to find out about your user base a conduct user disocovery to understand them</p>
+            </li>
+            <li>
+              <h3>Develop</h3>
+              <p>Before I start a project I like to find out about your user base a conduct user disocovery to understand them</p>
+            </li>
+            <li>
+              <h3>Testing</h3>
+              <p>Before I start a project I like to find out about your user base a conduct user disocovery to understand them</p>
+            </li>
+            <li>
+              <h3>Deploy &#x26; Monitor</h3>
+              <p>Before I start a project I like to find out about your user base a conduct user disocovery to understand them</p>
+            </li>
+          </ul>
+        </div>
+        <h2>The solution</h2>
+      </div>
     </div>
   </section>
 </template>
@@ -34,21 +62,13 @@ export default {
 <style scoped>
 section {
   grid-column: 2;
-  display: grid;
-  grid-template-columns: 1fr 2fr;
+  grid-template-columns: 1fr;
   grid-gap: 15px;
 
 }
 
 section div:first-of-type {
-  border-right: 1px solid rgba(150, 150, 150, 0.5);
-  grid-column: 1;
   padding: 15px;
-}
-
-section div h1 {
-  border-bottom: 1px solid rgba(150, 150, 150, 0.5);
-  grid-column: 1;
 }
 
 section div:nth-of-type(2) {
@@ -57,10 +77,20 @@ section div:nth-of-type(2) {
 
 @media only screen and (min-width: 1024px) {
     section {
-        display: grid;
-        grid-template-columns: 1fr 2fr;
-        grid-gap: 20px;
+      display: grid;
+      grid-template-columns: .5fr 2fr;
+      grid-gap: 20px;
     }
+
+    section div:first-of-type {
+      border-right: 1px solid rgba(150, 150, 150, 0.5);
+      padding: 15px;
+    }
+
+    section div h1 {
+      border-bottom: 1px solid rgba(150, 150, 150, 0.5);
+    }
+
  }
 
 </style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,36 +2,6 @@
   <section>
     <h1>Projects</h1>
     <p>A repository of some of my recent project work with links to the live site and GitHub repo. Also check out my development workflow below.</p>
-    <div id="workflow">
-      <div>
-        <h2>My Development Workflow</h2>
-        <p>My workflow is much more than just the development. Identifying your goals, understanding your user base to create the best product to solve their goals is how I like to do things.</p>
-      </div>
-      <div>
-        <ul>
-          <li>
-            <h3>Reserach &#x26; Dicovery</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi mollitia molestias omnis? Inventore praesentium quia doloremque soluta debitis veritatis sint, maiores accusamus explicabo, quae quod, molestias enim esse amet dolor?</p>
-          </li>
-          <li>
-            <h3>Design</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam cumque, dicta cupiditate debitis ad iure corrupti minus earum, harum culpa iste dolorem, itaque doloremque voluptates officiis aspernatur assumenda quae placeat!</p>
-          </li>
-          <li>
-            <h3>Develop</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, facilis veritatis totam voluptas voluptatum, harum eos pariatur impedit rem maxime repellendus exercitationem ipsum ipsa itaque dignissimos, obcaecati non. Autem, saepe.</p>
-          </li>
-          <li>
-            <h3>Testing</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cumque excepturi veritatis illum similique modi totam molestias recusandae, aut ratione necessitatibus natus, facere dolorum corrupti. Quibusdam dolor voluptatem similique molestias!</p>
-          </li>
-          <li>
-            <h3>Deploy &#x26; Monitor</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum maiores dignissimos dolore! Maiores quia molestias dolorum, unde nulla iste alias debitis a accusamus neque adipisci perferendis provident assumenda odit corrupti!</p>
-          </li>
-        </ul>
-      </div>
-    </div>
     <h2>Case Studies</h2>
     <figure
       v-for="project in projects"
@@ -39,14 +9,13 @@
       :title="project.title"
       class=""
     >
-      <h2>{{ project.title }}</h2>
+      <h3>{{ project.title }}</h3>
       <p>{{ project.detail }}</p>
-      <!-- <a href=" {{ project.link }} " /><i class="fab fa-github" /> -->
+      <NuxtLink to="/projects/casestudy1">
+        View case study
+      </NuxtLink>
     </figure>
     Project card and project list componets impo
-    <NuxtLink to="/projects/casestudy1">
-      View case study
-    </NuxtLink>
   </section>
 </template>
 
@@ -61,14 +30,16 @@ export default {
           title: 'Doggie Walks',
           detail: 'A real life client brief to design and devlop a statis website for a local dog walking business',
           link: 'https://github.com/clarkenewsh/doggie-walks-site',
-          thumbnail: ''
+          thumbnail: '',
+          path: '/projects/casestudy1'
         },
         {
           id: 2,
           title: 'Responsive Website UI Tester Tool',
           detail: 'A real life client brief to create a esponsive Website UI Tester Tool',
           link: 'https://github.com/clarkenewsh/Uni-Tap',
-          thumbnail: ''
+          thumbnail: '',
+          path: '/projects/casestudy2'
         }
 
       ]
@@ -97,8 +68,7 @@ figure {
 }
 
 #workflow {
-  display: grid;
-  grid-template-columns: 1fr 1.5fr;
+  grid-template-columns: 1fr;
   grid-column: 1/-1;
   list-style: none;
 }
@@ -106,22 +76,23 @@ figure {
 #workflow ul {
   list-style: none;
   padding: 0;
+  display: grid;
+  grid-template-columns: 1fr;
 }
 
 #workflow h2:nth-of-type(1) {
-  border-bottom: 1px solid rgba(150, 150, 150, 0.5);
   padding-bottom: 15px;
-}
-
-#workflow div:first-of-type {
-  grid-column: 1;
-  border-right: 1px solid rgba(150, 150, 150, 0.5);
-  padding-right: 15px;
 }
 
 #workflow div:nth-of-type(2){
   grid-column: 2;
-  padding: 15px;
+}
+
+@media only screen and (min-width: 1024px) {
+  #workflow ul {
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 30px;
+  }
 }
 
 </style>
