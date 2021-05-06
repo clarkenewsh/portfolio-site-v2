5bb8fcbff85f91f807fe275a10acb820632a6e11
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-05-06 19:44:06 +0100

    refactor

--- layouts/default.vue
+++ layouts/default.vue
@@ -33,15 +33,14 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+
 }
 
 *,
 *:before,
 *:after {
   box-sizing: border-box;
-  /* margin: 0; */
   line-height: 1.8;
-
 }
 
 body {
@@ -51,8 +50,8 @@ body {
 /* Mobile first css grid styles */
 .grid {
   display: grid;
-  grid-template-columns: 3% 1fr 3%;
-  grid-row-gap: 3rem;
+  grid-template-columns: 3.5% 1fr 3.5%;
+  grid-row-gap: 1rem;
   margin: 0 auto;
 }
 
@@ -62,6 +61,28 @@ main {
 
 header, main, footer {
   grid-column: 2;
+}
+
+a {
+  font-weight: 500;
+}
+
+button {
+  padding: 10px 15px;
+  border-radius: 0.375rem;
+  font-weight: 700;
+  margin: 10px 0px;
+  background: #ccd8d9;
+  border: none;
+  transition: all 0.2s ease;
+}
+
+button a {
+  color: #000;
+}
+
+button:hover {
+  transform: translateY(4px);
 }
 
 /*
@@ -77,50 +98,44 @@ h2 {
   font-weight: 700;
 }
 
- h3, h4 {
+ h3, h4, h5, h6 {
   color: #000;
   padding: 15px 0px;
-  font-weight: 500;
+  font-weight: 700;
 }
-
-/* @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap');
-
-p {
-  font-family: 'Roboto Mono', monospace;
-} */
 
 @media only screen and (min-width: 768px) {
   html {
     margin: 0;
   }
   .grid {
-    grid-template-columns: 3% 1fr 3%;
+    grid-template-columns: 3.5% 1fr 3.5%;
     grid-gap: 20px;
   }
 
   h1 {
-    font-size: 290%;
+    font-size: 320%;
   }
 
-  h2 {
-  font-size: 230%;
+   h2 {
+  font-size: 220%;
   }
+  /* h3 {
+  font-size: 135%;
+  } */
 }
 
 @media only screen and (min-width: 1024px) {
   html {
-    font-size: 140%;
+    font-size: 120%;
   }
   .grid {
     grid-template-columns: 5% 1fr 5%;
   }
-  h2 {
-    font-size: 250%;
+   h2 {
+    font-size: 260%;
   }
 
-  main{
-    padding: 40px 0px;
-  }
  }
 
 @media only screen and (min-width: 1400px) {
