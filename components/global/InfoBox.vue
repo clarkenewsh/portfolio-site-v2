1ff215a3cb8f0169f63456867bd319a047de0e31
clarke newsham <clarkenewsham@clarkes-mbp-2.mynet>
2021-05-17 19:24:45 +0100

    glibal componet folder directory created with info box components needed for blog

---
+++ components/global/InfoBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <p>
+      <slot name="info-box">
+        default
+      </slot>
+    </p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
