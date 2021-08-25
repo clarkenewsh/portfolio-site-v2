f24cc20c9c6df9fc5647add1f360b6863cc0e825
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2021-08-25 13:15:46 +0100

    typo fix

--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,9 @@
     <div id="about-me">
       <h2>A little about me</h2>
       <div class="split-block">
-        <p>Hey, I'm Clarke Newsham, aspiring User Researcher from the North West, UK. I'm super passionate about technology and how we can embrace it to develop great things for humanity.</p>
+        <p>Hey, I'm Clarke Newsham, from the North West, UK. I'm super passionate about technology and how we can embrace it to develop great things for humanity.</p>
         <p>Curiosity and futuristic thinking are what keeps me ambitious to design and develop web apps accessible to all, using the skills and techniques I have learned over the last 4 years studying at Edge Hill University. I love to stay creative and up to date with emerging technologies, development and reserach techniques by getting hands dirty building side projects and University studies.</p>
-        <p>I am passionate about building a career as a user researcher and have gained significant practical experience with my University work and my side-projects. I have a particular passion for automation testing and have gained competence in translating product requirements into user stories, creating test strategies, defining acceptance criteria and test scenarios. I will continue to learn throughout this year through continual learning and engaging in online courses and personal passion projects. </p>
+        <p>I am passionate about building a career as a user researcher and have gained significant practical experience of this with my University work and my side-projects. I will continue to learn throughout this year through continual learning and engaging in online courses and personal passion projects. </p>
       </div>
       <NuxtLink to="/about">
         <button>Learn more about me</button>
@@ -44,7 +44,7 @@
               <path d="M0 12.995V13a3.07 3.07 0 0 0 0-.005z" />
             </svg>
             <h4>User Research &#x26; UX Design</h4>
-            <p>Applying UX design models &#x26; techniques to develop engaging, user-friendly interfaces</p>
+            <p>Understanding users, applying UX design models &#x26; techniques to develop engaging, user-friendly interfaces</p>
           </li>
           <li>
             <svg
