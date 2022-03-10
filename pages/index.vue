6bacde02850d5bc06d0dff49a078945b77f64071
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-03-10 17:50:08 +0000

    about me body copy updated

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@
       <h2>A little about me</h2>
       <div class="split-block">
         <p>Hey, I'm Clarke Newsham, from the North West, UK. I am full of curiosity with a genuine passion for technology and how we can embrace it to develop great things for humanity.</p>
-        <p>Curiosity and creative problem solving is what keeps me ambitious to design and develop web apps that provide value to people and are accessible to all by using the skills and experience gained at Code Nation Masters Bootcamp, developing side projects and studying BSc Web Development at Edge Hill University. I also love to keep up to date with emerging web technologies and usability testing techniques by building side projects.</p>
+        <p>I'm full of curiosity with a passion to solve real-world problems through Web Development with a passion to develop web apps that provide value to people and are accessible to all. Throughout university, undertaking a Bootcamp, along with my passion for side-projects, I have gained practical experience in developing and testing web apps using a modern tech stack. I also love to keep up to date with emerging web technologies and usability testing techniques by building side projects.</p>
         <p>I am currently completing a Full Stack Web Development Masters Bootcamp with Code Nation were I am learning to develop full-stack web applications within development teams, using agile principles utilising a modern tech stack of MongoDB, Express, React and NodeJs.</p>
       </div>
       <!-- <NuxtLink to="/about">
